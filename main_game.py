6f28bdcd37b115e70f5c8501f54ebbe5720de417
Jim Renwick <peter.j.renwick@aexp.com>
2018-03-13 23:03:13 +0000

    completely refactored to make more object orientated

--- main_game.py
+++ main_game.py
@@ -5,11 +5,6 @@ from random import *
 import time
 from apscheduler.schedulers.background import BackgroundScheduler
 
-sched = BackgroundScheduler()
-sched.start()
-
-##SONG_PATH = "Neelix.mp3"
-##WAVE_PATH = "wave.png"
 SCREEN_X = 1920
 SCREEN_Y = 1080;
 
@@ -22,9 +17,42 @@ BLUE= ( 22, 148, 206)
 OFF_BLUE = (16, 127, 178)
 SCROLL_SPEED = 0.1
 OFF_BLACK = (60, 60, 60)
-SCORE = 0
+
+pygame.init()
+size = (SCREEN_X, SCREEN_Y)
+screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
+pygame.display.set_caption("Space Boy XTreme")
+clock = pygame.time.Clock()
+
+# play background tune
+ROCKET_MAN_SONG_PATH = 'rocket-man.mp3'
+pygame.mixer.pre_init(44100, -16, 2, 2048) # setup mixer to avoid sound lag
+pygame.mixer.init()
+pygame.mixer.music.load(ROCKET_MAN_SONG_PATH)
+#pygame.mixer.music.play(-1)
+
+# --- Limit to 60 frames per second
+clock.tick(60)
+
+# game font, 28pt
+FONT_PATH = "Font/Novecento WideDemiBold.otf"
+myfont = pygame.font.Font(FONT_PATH, 28)
+
+class ScoreCounter:
+    
+    def __init__(self, font):
+        self.score = 0
+        self.font = font
+
+    def update(self, increment):
+        self.score += increment
+
+    def draw(self):
+        screen.blit(self.font.render(str(self.score), 1, WHITE), (40, 40))
+        
 
 class Planet:
+    
     def __init__(self, posx, posy, imagePath, speed, randomHeight = False):
         self.posx = posx
         self.posy = posy
@@ -48,54 +76,64 @@ class Planet:
         if (self.randomHeight):
             self.posy = randint(0, SCREEN_Y - self.height)
 
-class Moon:
-    def __init__(self, posx, posy, imagePath, speed):
-        self.posx = posx
-        self.posy = posy
-        self.sprite = pygame.image.load(imagePath).convert_alpha()
-        self.width = self.sprite.get_width();
-        self.height = self.sprite.get_height();
-        self.speed = speed
+class SmallRedPlanet(Planet):
 
-    def move(self):
-        if (self.posx + self.width <= 0):
-            self.reset()
-        else:
-            self.posx -= self.speed
+    SMALL_PLANET_RED_PATH = "planet-dot-1.png"
+    
+    def __init__(self, posx, posy, speed, randomHeight = False):
+        Planet.__init__(self, posx, posy, self.SMALL_PLANET_RED_PATH, speed, randomHeight)
 
-    def draw(self):
-##        pygame.draw.rect(screen, RED, [self.posx, self.posy, self.width, self.height],0)
-        screen.blit(self.sprite, [self.posx, self.posy, self.width, self.height])
+class SmallBluePlanet(Planet):
 
-    def reset(self):
-        self.posx = randint(SCREEN_X, SCREEN_X+200)
-        self.posy = randint(0, SCREEN_Y - self.height)
+    SMALL_PLANET_BLUE_PATH = "planet-dot-2.png"
+    
+    def __init__(self, posx, posy, speed, randomHeight = False):
+        Planet.__init__(self, posx, posy, self.SMALL_PLANET_BLUE_PATH, speed, randomHeight)
+
+class LargePlanet(Planet):
+
+    LARGE_PLANET_PATH = "planet-lg.png"
+    
+    def __init__(self, posx, posy, speed, randomHeight = False):
+        Planet.__init__(self, posx, posy, self.LARGE_PLANET_PATH, speed, randomHeight)
+
+
+class Moon(Planet):
+
+    MOON_PATH = "moon.png"
+    
+    def __init__(self, posx, posy, speed):
+        Planet.__init__(self, posx, posy, self.MOON_PATH, speed, True)
 
     def isCollided(self, kid):
         moonMask = pygame.mask.from_surface(self.sprite)
         kidMask = pygame.mask.from_surface(kid.sprite)
         return not(kidMask.overlap(moonMask,(int(self.posx - kid.posx), int(self.posy - kid.posy))) == None)
-##        # if the start is inside then there's a collision
-##        return ((kid.posy <= self.posy <= (kid.posy+kid.height)
-##            and kid.posx <= self.posx <= (kid.posx + kid.width)) or
-##        # if the start is above but end inside or below then there's a collision
-##        (self.posy <= kid.posy and (self.posy + self.height) >= kid.posy
-##            and kid.posx <= self.posx <= (kid.posx + kid.width)))
 
 class Edible:
-    def __init__(self, posx, posy, imagePath, pointsValue):
+
+    EDIBLE_LOLLY_PATH = 'lolly.png'
+    EDIBLE_POPSICLE_PATH = 'pop.png'
+    EDIBLE_PINK_DOUGHNUT_PATH = 'donut-2.png'
+    EDIBLE_PLAIN_DOUGHNUT_PATH = 'donut.png'
+
+    PATHS = [EDIBLE_LOLLY_PATH, EDIBLE_POPSICLE_PATH, EDIBLE_PINK_DOUGHNUT_PATH, EDIBLE_PLAIN_DOUGHNUT_PATH]
+    SCORES = [100, 20, 200, 50]
+    
+    def __init__(self, posx, posy):
         self.posx = posx
         self.posy = posy
-        self.sprite = pygame.image.load(imagePath).convert_alpha()
+        self.speed = randint(3,10)
+        self.index = randint(0,len(self.PATHS)-1)
+        self.sprite = pygame.image.load(self.PATHS[self.index]).convert_alpha()
+        self.pointsValue = self.SCORES[self.index]
         self.width = self.sprite.get_width();
         self.height = self.sprite.get_height();
-        self.speed = randint(3,10)
-        self.pointsValue = pointsValue
 
-    def move(self, kid):
+    def move(self, kid, level):
         if (self.isCollided(kid)):
-            global floatingPoints
-            floatingPoints.append(FloatingPoint(self.posx, self.posy, self.pointsValue))
+            level.score.update(self.pointsValue)
+            level.floatingPoints.append(FloatingPoint(self.posx, self.posy, self.pointsValue))
             self.reset()
         elif (self.posx + self.width <= 0):
             self.reset()
@@ -108,8 +146,13 @@ class Edible:
     def reset(self):
         self.posx = randint(SCREEN_X, SCREEN_X+200)
         self.posy = randint(0, SCREEN_Y - self.height)
-        global SCORE
-        SCORE += self.pointsValue
+        self.index = randint(0,len(self.PATHS)-1)
+        self.sprite = pygame.image.load(self.PATHS[self.index]).convert_alpha()
+        self.pointsValue = self.SCORES[self.index]
+        self.speed = randint(3,10)
+        self.width = self.sprite.get_width();
+        self.height = self.sprite.get_height();
+
 
     def isCollided(self, kid):
         # if the start is inside then there's a collision
@@ -133,7 +176,7 @@ class FloatingPoint:
         self.countdown += 1
 
     def draw(self):
-        # lower countdown = weaker image, when alpha is 0, self construct
+        ## TODO try to refactor this
         label = myfont.render(str(self.value), 1, WHITE)
         surface=pygame.Surface(label.get_size())
         surface.convert_alpha().fill((0, 0, 0, 0))
@@ -169,11 +212,35 @@ class Background:
             screen.blit(self.sprite, (0, 0))
             self.counter = SCREEN_X
 
+class LargeStarBackground(Background):
+
+    LARGE_STAR_PATH = "start-lg.png"
+
+    def __init__(self, speed):
+        Background.__init__(self, self.LARGE_STAR_PATH, speed)
+
+class MediumStarBackground(Background):
+
+    MEDIUM_STAR_PATH = "start-md.png"
+
+    def __init__(self, speed):
+        Background.__init__(self, self.MEDIUM_STAR_PATH, speed)
+
+class SmallStarBackground(Background):
+
+    SMALL_STAR_PATH = "start-xs.png"
+
+    def __init__(self, speed):
+        Background.__init__(self, self.SMALL_STAR_PATH, speed)
+
+
 class SpaceKid:
 
     FLAME_1_PATH = 'flames/flame-1.png'
     FLAME_2_PATH = 'flames/flame-2.png'
     FLAME_3_PATH = 'flames/flame-3.png'
+
+    SPACE_KID_PATH = "space-kid.png"
 
     
     def __init__(self, posx, posy):
@@ -181,7 +248,7 @@ class SpaceKid:
         self.posy = posy
         self.speed = 20
         # load sprite and resize it to match the requested dimensions
-        self.sprite = pygame.image.load(SPACE_KID_PATH).convert_alpha()
+        self.sprite = pygame.image.load(self.SPACE_KID_PATH).convert_alpha()
         self.width = self.sprite.get_width();
         self.height = self.sprite.get_height();
         self.frames = []
@@ -231,6 +298,106 @@ class ProgressBar:
                                          self.width,
                                          math.ceil(self.height / self.totalSize * self.progress)],0)
 
+class Level:
+
+    def __init__(self, levelNumber, font):
+        self.levelNumber = levelNumber
+        self.score = ScoreCounter(font)
+
+        self.floatingPoints = []
+
+        # create backdrop objects behind space kid
+        self.backgrounds = []
+        self.backgrounds.append(SmallStarBackground(0.7))
+        self.backgrounds.append(MediumStarBackground(1))
+        self.backgrounds.append(LargeStarBackground(1.5))
+        
+        self.bkgrdPlanets = []
+        self.bkgrdPlanets.append(LargePlanet(SCREEN_X, SCREEN_Y / 4, 2))
+        self.bkgrdPlanets.append(SmallRedPlanet(randint(SCREEN_X, SCREEN_X+200), randint(0, SCREEN_Y), 3, randomHeight=True))
+        self.bkgrdPlanets.append(SmallBluePlanet(randint(SCREEN_X, SCREEN_X+200), randint(0, SCREEN_Y), 3, randomHeight=True))
+
+        self.edibles = []
+        self.edibles.append(Edible(randint(SCREEN_X, SCREEN_X+200), randint(0, SCREEN_Y)))
+        self.edibles.append(Edible(randint(SCREEN_X, SCREEN_X+200), randint(0, SCREEN_Y)))
+        self.edibles.append(Edible(randint(SCREEN_X, SCREEN_X+200), randint(0, SCREEN_Y)))
+        self.edibles.append(Edible(randint(SCREEN_X, SCREEN_X+200), randint(0, SCREEN_Y)))
+
+        # create planet objects in front of space kid - these collide with him
+        self.moons = []
+        self.moons.append(Moon(SCREEN_X, randint(0, SCREEN_Y), 25))
+
+        self.kid = SpaceKid(55, 200)
+
+        # start scheduled job to generate new moon every 10 seconds
+        self.sched = BackgroundScheduler()
+        self.sched.start()
+        self.sched.add_job(trigger='interval', seconds = 10, func=lambda: self.moons.append(Moon(SCREEN_X, randint(0, SCREEN_Y), 25)))
+
+    def checkInputs(self, events):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.kid.move(0,-1)
+                if event.key == pygame.K_DOWN:
+                    self.kid.move(0,1)
+                if event.key == pygame.K_RIGHT:
+                    self.kid.move(1,0)
+                if event.key == pygame.K_LEFT:
+                    self.kid.move(-1,0)
+                
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.kid.move(0,-1)
+        elif keys[pygame.K_DOWN]:
+            self.kid.move(0,1)
+        if keys[pygame.K_RIGHT]:
+            self.kid.move(1,0)
+        elif keys[pygame.K_LEFT]:
+            self.kid.move(-1,0)
+
+    def update(self, screen):
+        # update all level objects and, if game over, return false - otherwise return true
+        screen.fill(OFF_BLACK)
+        for background in self.backgrounds:
+            background.shiftLeft()
+
+        for planet in self.bkgrdPlanets:
+            planet.move()
+            planet.draw()
+
+        self.kid.draw()
+
+        for edible in self.edibles:
+            edible.move(self.kid, self)
+            edible.draw()
+
+        for moon in self.moons:
+            moon.move()
+            moon.draw()
+            if moon.isCollided(self.kid):
+                self.endLevel()
+                return False
+
+        for point in self.floatingPoints:
+            point.move()
+            if (point.shouldDestroy()):
+                self.floatingPoints.remove(point)
+            else:
+                point.draw()
+
+        self.score.draw()
+
+        return True
+
+    def endLevel(self):
+        self.sched.remove_all_jobs()
+        self.sched.shutdown()
+        showGameOverScreen()
+
+
 #### SCREEN SWITCHING/QUITTING
 
 def showGameOverScreen():
@@ -243,14 +410,10 @@ def showGameOverScreen():
     label = myfont.render("G A M E  O V E R", 1, WHITE)
     screen.blit(label, (SCREEN_X / 2 - (label.get_width() / 2), SCREEN_Y / 2 - (label.get_height() / 2)))
     pygame.display.flip()
-    
-def quitGame():
-    sched.shutdown()
+    time.sleep(2)
     pygame.quit()
 
-def endLevel():
-    sched.remove_all_jobs()
-    showGameOverScreen()
+#### UTILITY METHODS
 
 def translate(value, oldMin, oldMax, newMin, newMax):
     # Figure out how 'wide' each range is
@@ -264,149 +427,15 @@ def translate(value, oldMin, oldMax, newMin, newMax):
     return newMin + (valueScaled * rightSpan)
 
 
-#### ART ASSETS
-
-ROCKET_MAN_SONG_PATH = 'rocket-man.mp3'
-LARGE_STAR_PATH = "start-lg.png"
-SMALL_STAR_PATH = "start-xs.png"
-MEDIUM_STAR_PATH = "start-md.png"
-MOON_PATH = "moon.png"
-
-EDIBLE_LOLLY_PATH = 'lolly.png'
-EDIBLE_POPSICLE_PATH = 'pop.png'
-EDIBLE_PINK_DOUGHNUT_PATH = 'donut-2.png'
-EDIBLE_PLAIN_DOUGHNUT_PATH = 'donut.png'
-
-LARGE_PLANET_PATH = "planet-lg.png"
-SMALL_PLANET_RED_PATH = "planet-dot-1.png"
-SMALL_PLANET_BLUE_PATH = "planet-dot-2.png"
-
-SPACE_KID_PATH = "space-kid.png"
-
-FONT_PATH = "Font/Novecento WideDemiBold.otf"
-
-##GAME_OVER_TEXT_PATH = 
-
-
-## INITIALISE GAME
-
-pygame.init()
-size = (SCREEN_X, SCREEN_Y)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Space Boy XTreme")
-
 
 carryOn = True
-# The clock will be used to control how fast the screen updates
-clock = pygame.time.Clock()
-# play background tune
-pygame.mixer.pre_init(44100, -16, 2, 2048) # setup mixer to avoid sound lag
-pygame.mixer.init()
-pygame.mixer.music.load(ROCKET_MAN_SONG_PATH)
-#pygame.mixer.music.play(-1)
-
-# --- Limit to 60 frames per second
-clock.tick(60)
-
-## TODO add this to the background planets array
-large_planet_bkgd = pygame.image.load(LARGE_PLANET_PATH).convert_alpha()
-
-floatingPoints = []
-
-# create planet objects behind space kid
-bkgrdPlanets = []
-bkgrdPlanets.append(Planet(SCREEN_X, SCREEN_Y / 4, LARGE_PLANET_PATH, 2))
-bkgrdPlanets.append(Planet(randint(SCREEN_X, SCREEN_X+200), randint(0, SCREEN_Y), SMALL_PLANET_RED_PATH, 3, randomHeight=True))
-bkgrdPlanets.append(Planet(randint(SCREEN_X, SCREEN_X+200), randint(0, SCREEN_Y), SMALL_PLANET_BLUE_PATH, 3, randomHeight=True))
-
-edibles = []
-edibles.append(Edible(randint(SCREEN_X, SCREEN_X+200), randint(0, SCREEN_Y), EDIBLE_LOLLY_PATH, 20))
-edibles.append(Edible(randint(SCREEN_X, SCREEN_X+200), randint(0, SCREEN_Y), EDIBLE_POPSICLE_PATH, 10))
-edibles.append(Edible(randint(SCREEN_X, SCREEN_X+200), randint(0, SCREEN_Y), EDIBLE_PLAIN_DOUGHNUT_PATH, 50))
-edibles.append(Edible(randint(SCREEN_X, SCREEN_X+200), randint(0, SCREEN_Y), EDIBLE_PINK_DOUGHNUT_PATH, 100))
-
-# create planet objects in front of space kid - these collide with him
-moons = []
-moons.append(Moon(SCREEN_X, randint(0, SCREEN_Y),MOON_PATH, 25))
-
-# create backdrop objects
-backgrounds = []
-backgrounds.append(Background(SMALL_STAR_PATH, 0.7))
-backgrounds.append(Background(MEDIUM_STAR_PATH, 1))
-backgrounds.append(Background(LARGE_STAR_PATH, 1.5))
-
-kid = SpaceKid(55, 200)
-
-# set font
-myfont = pygame.font.Font(FONT_PATH, 28)
-
-## TODO add this and main loop into a standalone method
-sched.add_job(trigger='interval', seconds = 10, func=lambda: moons.append(Moon(SCREEN_X, randint(0, SCREEN_Y),MOON_PATH, 25)))
 
 # MAIN PROGRAM LOOP
 ##progressBar = ProgressBar(SCREEN_X-40, 10, 30, SCREEN_Y-20, 1000)
+level = Level(1, myfont)
 while carryOn:
-    #clock.tick()
-    # Check inputs
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quitGame()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                kid.move(0,-1)
-            if event.key == pygame.K_DOWN:
-                kid.move(0,1)
-            if event.key == pygame.K_RIGHT:
-                kid.move(1,0)
-            if event.key == pygame.K_LEFT:
-                kid.move(-1,0)
-                
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
-        kid.move(0,-1)
-    elif keys[pygame.K_DOWN]:
-        kid.move(0,1)
-    if keys[pygame.K_RIGHT]:
-        kid.move(1,0)
-    elif keys[pygame.K_LEFT]:
-        kid.move(-1,0)
-        
-    # Move background
-    screen.fill(OFF_BLACK)
-    for background in backgrounds:
-        background.shiftLeft()
-
-    for planet in bkgrdPlanets:
-        planet.move()
-        planet.draw()
-
-    kid.draw()
-
-    for edible in edibles:
-        edible.move(kid)
-        edible.draw()
-
-    for moon in moons:
-        if moon.isCollided(kid):
-            carryOn = False
-            endLevel()
-        moon.move()
-        moon.draw()
-
-    for point in floatingPoints:
-        point.move()
-        if (point.shouldDestroy()):
-            floatingPoints.remove(point)
-        else:
-            point.draw()
-                                                            
-                                                              
-
-    # draw score
-    label = myfont.render(str(SCORE), 1, WHITE)
-    screen.blit(label, (40, 40))
-
-    # render to screen
+    level.checkInputs(pygame.event.get())
+    carryOn = level.update(screen)
     pygame.display.flip()
 
 
